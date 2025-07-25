08b1abf46b73803f3818500783f70682c4249638
Ivo Santiago <ivo.santiago@appcues.com>
2025-07-25 16:17:04 -0300

    create confirmation screen

---
+++ src/components/confirmation.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+import { ClinicCard } from "./clinic-card";
+import { Card } from "./ui/card";
+
+export function Confirmation() {
+  return (
+    <div className="p-8">
+      <div className="mx-auto flex max-w-screen-xl flex-col gap-2 pb-[100px] md:pb-0">
+        <h1 className="self-center text-lg font-bold md:self-auto md:pl-2">
+          Book appointment
+        </h1>
+        <div className="flex flex-col items-center gap-6">
+          <div className="w-full text-center md:w-[542px]">
+            <Card className="gap-6 bg-white px-[60px] py-10">
+              <Image
+                className="self-center"
+                src={"/success-icon-with-circle.svg"}
+                alt="Confirmation checkmark in a green circle"
+                width={200}
+                height={200}
+              />
+              <h4 className="text-mx-text-description text-lg font-bold">
+                Your appointment has been booked!
+              </h4>
+              <p className="text-mx-text-dark">
+                A confirmation has been sent to your email address.
+              </p>
+            </Card>
+          </div>
+
+          <div className="w-full md:w-[542px]">
+            <ClinicCard />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
